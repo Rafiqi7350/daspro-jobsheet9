59796fc4175f8998d40eba62034c16c305c58e2d
Rafiqi7350 <muhammadrasyid7350@gmail.com>
2024-10-29 11:27:00 +0700

    Array Bilangan

---
+++ ArrayBilanganModifikasi09.java
@@ -0,0 +1,17 @@
+public class ArrayBilanganModifikasi09 {
+
+    public static void main(String[] args) {
+       
+    //array bertipe int
+        int [] bil = new int[4];
+
+        bil [0] = 5;
+        bil [1] = 13;
+        bil [2] = -7;
+        bil [3] = 17;
+//Menggunakan for
+    for (int i =0; i<4; i++) {
+        System.out.println(bil[i]);
+    }
+  }
+}
